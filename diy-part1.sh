75e5a389542fbc52740284eacafb09c1ec4d8b2c
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2021-06-26 15:07:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+mkdir package/base-files/files/config
+echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+cd package
+mkdir openwrt-packages
+cd openwrt-packages
+git clone https://github.com/tty228/luci-app-serverchan
+git clone https://github.com/nb12nb34/luci-theme-atmaterial-1
+cd ../..
+./scripts/feeds update -a && ./scripts/feeds install -a 

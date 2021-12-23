803874720569ae29f06628607ef13cbe36451e2a
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2021-12-23 13:32:02 +0800

    Update diy-5.10.sh

--- diy-5.10.sh
+++ diy-5.10.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+#=============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
 
-# Modify default IP
 sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+sed -i '$a src-git ssr https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git ssr https://github.com/nb12nb34/luci-theme-atmaterial-1' feeds.conf.default
 sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+
+

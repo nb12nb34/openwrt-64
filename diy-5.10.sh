8fc4417a89fd8fc07d5d3f4fe395c98901cd969f
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2021-12-23 15:54:45 +0800

    Update diy-5.10.sh

--- diy-5.10.sh
+++ diy-5.10.sh
@@ -14,6 +14,6 @@ sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makef
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-git clone https://github.com/nb12nb34/luci-theme-atmaterial-1.git package/luci-theme-atmaterial
+git clone https://github.com/nb12nb34/luci-theme-atmaterial.git package/luci-theme-atmaterial
 
 

06db920bed283c873d3003fada5c03fc4fd72a90
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2021-12-23 13:39:45 +0800

    Update diy-5.4.sh

--- diy-5.4.sh
+++ diy-5.4.sh
@@ -8,8 +8,10 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-sed -i '$a src-git ssr https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git ssr https://github.com/nb12nb34/luci-theme-atmaterial-1' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git ssr https://github.com/nb12nb34/luci-theme-atmaterial-1' >>feeds.conf.default
 sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 

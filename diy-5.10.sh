a664dc0e82763ee55da48f7fd391822be230f8ee
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2021-12-23 13:50:41 +0800

    Update diy-5.10.sh

--- diy-5.10.sh
+++ diy-5.10.sh
@@ -9,9 +9,11 @@
 #=============================================================
 
 sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
-sed -i '$a src-git ssr https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git ssr https://github.com/nb12nb34/luci-theme-atmaterial-1' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/nb12nb34/luci-theme-atmaterial-1.git package/luci-theme-atmaterial
 
 

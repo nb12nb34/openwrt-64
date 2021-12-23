f6b9dacaaecd199b4a4c6b99206eefd592a680d2
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2021-12-23 13:27:50 +0800

    Update and rename diy-part1.sh to diy-part2.sh

--- diy-part1.sh
+++
@@ -1,3 +0,0 @@
-sed -i '$a src-git ssr https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git ssr https://github.com/nb12nb34/luci-theme-atmaterial-1' feeds.conf.default
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan

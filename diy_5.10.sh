cb62caec9247a7b2e7a8a8370242f17d1826c2db
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2022-01-08 10:35:16 +0800

    Update diy_5.10.sh

--- diy_5.10.sh
+++ diy_5.10.sh
@@ -12,6 +12,5 @@ sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-# git clone https://github.com/nb12nb34/tencentcloud-openwrt-plugin-ddns.git package/luci-app-tencentddns
-git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
+git clone https://github.com/nb12nb34/tencentcloud-openwrt-plugin-ddns.git package/luci-app-tencentddns
 git clone https://github.com/nb12nb34/luci-theme-atmaterial.git package/luci-theme-atmaterial

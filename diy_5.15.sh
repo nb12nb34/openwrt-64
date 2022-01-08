18b40b7b3e112ee5093eb093955c740dd65e4221
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2022-01-08 10:37:40 +0800

    Update diy_5.15.sh

--- diy_5.15.sh
+++ diy_5.15.sh
@@ -9,11 +9,11 @@
 #=============================================================
 
 sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/nb12nb34/tencentcloud-openwrt-plugin-ddns.git package/luci-app-tencentddns
 git clone https://github.com/nb12nb34/luci-theme-atmaterial.git package/luci-theme-atmaterial
 
 

4815bd7bc67ea0fc492b7e9f51eefc8c80a0b9f1
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2023-07-10 07:56:37 +0800

    Update diy_default.sh

--- diy_default.sh
+++ diy_default.sh
@@ -10,5 +10,5 @@
 
 sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
 git clone https://github.com/nb12nb34/luci.git package/nb12nb34

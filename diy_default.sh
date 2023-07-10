ac565543a16ebab5a68cd7761ced9e4146e3832e
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2023-07-10 21:34:03 +0800

    Update diy_default.sh

--- diy_default.sh
+++ diy_default.sh
@@ -10,5 +10,5 @@
 
 sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git package/wechatpush
+git clone https://github.com/tty228/luci-app-wechatpush.git package/wechatpush
 git clone https://github.com/nb12nb34/luci.git package/nb12nb34

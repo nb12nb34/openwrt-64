d8ec983b2bb27abda833e4baba20a051545966aa
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2024-02-06 19:16:21 +0800

    Update diy_default.sh

--- diy_default.sh
+++ diy_default.sh
@@ -10,7 +10,6 @@
 
 # git checkout a2a0e5b
 sed -i "/helloworld/d" "feeds.conf.default"
-sed -i 's/stripped/release/g' feeds/packages/multimedia/aliyundrive-webdav/Makefile
 echo "src-git helloworld https://github.com/fw876/helloworld;main" >> "feeds.conf.default"
 # git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git package/wechatpush
 git clone https://github.com/nb12nb34/luci.git package/nb12nb34

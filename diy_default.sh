b607909a0c3ba187e40ebc71c1949246ab491479
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2024-12-26 15:09:35 +0800

    Update diy_default.sh

--- diy_default.sh
+++ diy_default.sh
@@ -4,11 +4,9 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 # Lisence: MIT
-# Author: P3TERXhttps://github.com/nb12nb34/openwrt-64/blob/main/diy-5.4.sh
 # Blog: https://p3terx.com
 #=============================================================
 
-# git checkout a2a0e5b
 sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld;main" >> "feeds.conf.default"
 # git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git package/wechatpush

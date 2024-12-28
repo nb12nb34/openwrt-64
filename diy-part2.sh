9d552f58daf384e3e890e5cb2a0fc55c6a1083f3
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2024-12-28 15:23:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,7 @@
 #============================================================
 
 sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/files/bin/config_generate
+sed -i 's/luci-theme-bootstrap/luci-theme-opentomcat/g' ./feeds/luci/collections/luci/Makefile
 chmod -R 755 ./package/nb12nb34/
 # rm -rf ./feeds/luci/applications/luci-app-serverchan
 # rm -rf ./feeds/luci/applications/luci-app-dockerman

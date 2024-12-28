c94163f5c7e10ec052b9853ce60be0fe1436585b
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2024-12-29 07:27:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,9 +9,10 @@
 #============================================================
 git clone https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon
 git clone -b js https://github.com/sirpdboy/luci-theme-kucat.git  package/luci-theme-kucat
-# git clone https://github.com/tty228/luci-app-wechatpush.git package/wechatpush
 sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/luci2/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 chmod -R 755 ./package/nb12nb34/
+rm -rf ./feeds/luci/applications/luci-app-wechatpush
+rm -rf ./feeds/luci/themes/luci-theme-argon
 # rm -rf ./feeds/luci/applications/luci-app-serverchan
 # rm -rf ./feeds/luci/applications/luci-app-dockerman

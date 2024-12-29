616e7c05ea83387556b3551fbafe6c8f643b7cb7
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2024-12-29 12:08:56 +0800

    Update diy_default.sh

--- diy_default.sh
+++ diy_default.sh
@@ -14,3 +14,4 @@
 git clone https://github.com/nb12nb34/luci.git package/nb12nb34
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone https://github.com/tty228/luci-app-wechatpush.git package/luci-app-serverchan

32b2be2db4b93d92b70a8fe3e6129a5f777d7bd5
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2024-12-31 21:34:14 +0800

    Update diy_6.1_18.6part2.sh

--- diy_6.1_18.6part2.sh
+++ diy_6.1_18.6part2.sh
@@ -11,5 +11,4 @@
 sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/files/bin/config_generate
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 chmod -R 755 ./package/nb12nb34/
-./scripts/feeds update helloworld
-./scripts/feeds install -a -f -p helloworld
+

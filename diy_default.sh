60eb7de52e41ef60f23e411cbbd1f214b7dceb84
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2024-12-31 16:37:41 +0800

    Update diy_default.sh

--- diy_default.sh
+++ diy_default.sh
@@ -9,3 +9,10 @@
 
 # sed -i "/helloworld/d" "feeds.conf.default"
 # echo "src-git helloworld https://github.com/fw876/helloworld;main" >> "feeds.conf.default
+
+git clone https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
+git clone https://github.com/nb12nb34/luci.git package/nb12nb34
+git clone https://github.com/tty228/luci-app-wechatpush.git package/luci-app-serverchan
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall

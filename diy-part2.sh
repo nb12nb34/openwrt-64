d77edfa753b162419d0fb3b470186e35d1a719e4
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2024-12-31 16:37:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,13 +7,6 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #============================================================
-git clone https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
-git clone https://github.com/nb12nb34/luci.git package/nb12nb34
-git clone https://github.com/tty228/luci-app-wechatpush.git package/luci-app-serverchan
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-
 
 sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/luci2/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile

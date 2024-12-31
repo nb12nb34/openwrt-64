d8b3640cf48d7553a96f552d2698c9ab88f874ba
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2024-12-31 16:38:42 +0800

    Update diy_6.1.sh

--- diy_6.1.sh
+++ diy_6.1.sh
@@ -14,6 +14,6 @@ sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makef
 git clone https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
 git clone https://github.com/nb12nb34/luci.git package/nb12nb34
-git clone https://github.com/tty228/luci-app-wechatpush.git package/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall

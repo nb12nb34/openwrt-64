faf72767f5dea79eb680ba7f778c53c9c94fa7b8
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2024-12-31 16:32:11 +0800

    Update diy_6.1.sh

--- diy_6.1.sh
+++ diy_6.1.sh
@@ -8,11 +8,9 @@
 # Blog: https://p3terx.com
 #=============================================================
 sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
-sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-# git clone https://github.com/fw876/helloworld.git package/ssr
-# git clone https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon
-# git clone -b js https://github.com/sirpdboy/luci-theme-kucat.git  package/luci-theme-kucat
+# sed -i "/helloworld/d" "feeds.conf.default"
+# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default
+#  git clone https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon
 git clone https://github.com/nb12nb34/luci.git package/nb12nb34
 git clone https://github.com/tty228/luci-app-wechatpush.git package/luci-app-serverchan
 # git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall

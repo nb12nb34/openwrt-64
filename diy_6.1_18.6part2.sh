82ea787edac4cf091dccb2fa9eefd09797cd8226
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2026-02-01 07:30:41 +0800

    Update diy_6.1_18.6part2.sh

--- diy_6.1_18.6part2.sh
+++ diy_6.1_18.6part2.sh
@@ -8,7 +8,8 @@
 # Blog: https://p3terx.com
 #============================================================
 
-sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/luci/bin/config_generate
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 chmod -R 755 ./package/nb12nb34/
 

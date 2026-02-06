30af80c3ea0eddc8d732eeb942abbf988a14a909
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2026-02-07 07:15:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,6 @@ chmod -R 755 ./package/nb12nb34/
 
 rm -rf ./feeds/luci/applications/luci-app-wechatpush
 rm -rf ./feeds/luci/themes/luci-theme-argon
-rm -rf ./feeds/luci/applications/luci-app-wechatpush
+# rm -rf ./feeds/luci/package/nb12nb34/luci-app-wechatpush
 # rm -rf ./feeds/luci/applications/luci-app-serverchan
 # rm -rf ./feeds/luci/applications/luci-app-dockerman

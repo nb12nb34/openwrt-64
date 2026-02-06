818b93a090966ff818404ec586632a4a98054b88
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2026-02-07 07:26:01 +0800

    Fix path for removing luci-app-poweroff

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,4 +15,4 @@ chmod -R 755 ./package/nb12nb34/
 
 rm -rf ./feeds/luci/applications/luci-app-wechatpush
 rm -rf ./feeds/luci/themes/luci-theme-argon
-rm -rf ./feeds/luci/package/nb12nb34/luci-app-poweroff
+rm -rf ./feeds/package/nb12nb34/luci-app-poweroff

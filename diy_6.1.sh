956e3d00b8ec1474af2a280b0101d20d5706a13b
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2026-02-08 09:08:05 +0800

    Add cloning of poweroffdevice package

--- diy_6.1.sh
+++ diy_6.1.sh
@@ -13,6 +13,7 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-
 git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
 git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/esirplayground/luci-app-poweroff.git  package/luci-app-poweroff
+git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git  package/luci-app-poweroffdevice
 git clone https://github.com/nb12nb34/luci.git package/nb12nb34
 git clone https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/openwrt-passwall

131e748d4c412252295df5e5863222c81ffd6131
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2026-02-10 22:08:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,10 +9,8 @@
 #============================================================
 
 sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/luci/bin/config_generate
-# sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-kucat/g' ./feeds/luci/collections/luci/Makefile
-# sed -i's/\"services\"/\"nas\"/g' ./feeds/luci/applications/luci-app-samba4/luasrc/controller/*.lua
-chmod -R 755 ./package/nb12nb34/
+chmod -R 755 ./package/nb12nb34
 
 rm -rf ./feeds/luci/applications/luci-app-wechatpush
 rm -rf ./feeds/luci/themes/luci-theme-argon

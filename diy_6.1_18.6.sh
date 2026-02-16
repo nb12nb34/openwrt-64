f7a8ce4f2c2c14d50615864f9f1a849d3dc947e9
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2026-02-16 13:27:33 +0800

    Update diy_6.1_18.6.sh

--- diy_6.1_18.6.sh
+++ diy_6.1_18.6.sh
@@ -13,7 +13,7 @@ sed -i '/openwrt-24.10/d' feeds.conf.default
 sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
 
 git clone https://github.com/fw876/helloworld.git package/ssr
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
 git clone https://github.com/nb12nb34/luci.git package/nb12nb34
 git clone https://github.com/esirplayground/luci-app-poweroff.git  package/luci-app-poweroff
